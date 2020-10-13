d17452d6267b0edda73ccd4a9725af5c1dbc1731
hanzo <carock21.arong@outlook.jp>
2020-10-13 11:03:18 +0000

    favorite button

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,5 +1,9 @@
 class Micropost < ApplicationRecord
   belongs_to :user
   
+  has_many :relationships
+  has_many :reverses_of_favorite, class_name: 'Favorite', foreign_key: 'micropost_id'
+  has_many :favoriting_users, through: :reverses_of_favorite, source: :user
+  
   validates :content, presence: true, length: { maximum: 255 }
 end
